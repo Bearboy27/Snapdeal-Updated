98b571279951cd8c6c40ddf204ed10aa3c97d70f
bearboy27 <appalahemanth413@gmail.com>
2023-04-09 13:08:03 +0530

    structuring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// this is a function component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="a_header_top">
+          <div className="container"></div>
+        </div>
+        <div className="a_header_bottom">
+          <div className="container"></div>
+        </div>
       </header>
+      <div className="a_main pb-5">
+        <div className="container a_main1 mt-3"></div>
+        <div className="container-fluid a_main2"></div>
+      </div>
+      <footer>
+        <div className="container a_footer1"></div>
+        <div className="container a_footer2"></div>
+        <div className="container a_footer3"></div>
+      </footer>
     </div>
   );
 }
